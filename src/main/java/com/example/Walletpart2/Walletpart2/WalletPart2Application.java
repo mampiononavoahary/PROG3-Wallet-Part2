5e54d742f422620fc3d4fc232ddabcb2c54059b2
kareem Nomenjanahary <kareem@jitsi.yourdomain>
2023-12-07 11:58:55 +0300

    creation of the Spring Boot project

---
+++ src/main/java/com/example/Walletpart2/Walletpart2/WalletPart2Application.java
@@ -0,0 +1,13 @@
+package com.example.Walletpart2.Walletpart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletPart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletPart2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Walletpart2/Walletpart2/WalletPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Walletpart2.Walletpart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletPart2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
