02ff4f126fad635c97f081a2411d7ebdc4adffa1
Voahary Mampionona <hei.zoarisoa@gmail.com>
2023-12-08 17:49:34 +0300

    feat: call tests in WalletPart2Application

--- src/main/java/com/example/Walletpart2/Walletpart2/WalletPart2Application.java
+++ src/main/java/com/example/Walletpart2/Walletpart2/WalletPart2Application.java
@@ -1,5 +1,7 @@
 package com.example.Walletpart2.Walletpart2;
 
+import com.example.Walletpart2.Walletpart2.Tests.DevisesTests;
+import com.example.Walletpart2.Walletpart2.Tests.TransactionTests;
 import com.example.Walletpart2.Walletpart2.Tests.compteTests;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,8 +13,11 @@ public class WalletPart2Application {
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		compteTests compteTests = new compteTests();
-
-		compteTests.compteTets();
+		DevisesTests devisesTests = new DevisesTests();
+		TransactionTests transactionTests = new TransactionTests();
+		TransactionTests.transactionTest();
+//		DevisesTests.DevisesTest();
+//
 	}
 
 }
