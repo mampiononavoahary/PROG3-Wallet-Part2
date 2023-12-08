928fc9b90e2eb816ae4097778c30a0acd87af826
Voahary Mampionona <hei.zoarisoa@gmail.com>
2023-12-08 06:46:34 +0300

    feat:add the CRUD of devises

--- src/main/java/com/example/Walletpart2/Walletpart2/Repository/comptesCrudOperations.java
+++ src/main/java/com/example/Walletpart2/Walletpart2/Repository/comptesCrudOperations.java
@@ -4,10 +4,7 @@ import com.example.Walletpart2.Walletpart2.Database.ConnectDatabase;
 import com.example.Walletpart2.Walletpart2.Model.comptes;
 import com.example.Walletpart2.Walletpart2.Model.typeCompte;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,8 +20,8 @@ public class comptesCrudOperations implements CrudOperations<comptes>{
         String nomCompte = resultSet.getString("nom_de_compte");
         double solde = resultSet.getDouble("solde");
         LocalDateTime dateMisAJour = resultSet.getTimestamp("solde_date_mis_a_jour").toLocalDateTime();
-        int idDevise = resultSet.getInt("id_devise");
-        typeCompte type = typeCompte.valueOf(resultSet.getString("type"));
+        int idDevise = resultSet.getInt("devise_id");
+        typeCompte type = typeCompte.valueOf(resultSet.getString("type_compte"));
 
         return new comptes(id,nomCompte,solde,dateMisAJour,idDevise,type);
     }
@@ -38,7 +35,9 @@ public class comptesCrudOperations implements CrudOperations<comptes>{
             while (resultSet.next()){
                 comptes compte= extractResultset(resultSet);
                 comptes.add(compte);
-                System.out.println(compte);
+            }
+            for (comptes comptes1: comptes){
+                System.out.println(comptes1);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -50,7 +49,7 @@ public class comptesCrudOperations implements CrudOperations<comptes>{
     public List<comptes> saveAll(List<comptes> toSave) {
         List<comptes> arrayComptes = new ArrayList<>();
         try {
-            String sql = "INSERT INTO Comptes(id,nomCompte,solde,dateMisAJour,idDevise,type) VALUES(?,?,?,?,?,?);";
+            String sql = "INSERT INTO Comptes(id,nom_de_compte,solde,solde_date_mis_a_jour,devise_id,type_compte) VALUES(?,?,?,?,?,?);";
             getConnection();
             try (PreparedStatement statement = connection.prepareStatement(sql)){
                for (comptes compte: toSave){
@@ -59,7 +58,7 @@ public class comptesCrudOperations implements CrudOperations<comptes>{
                    statement.setDouble(3,compte.getSolde());
                    statement.setObject(4,compte.getSolde_date_mis_a_jour());
                    statement.setInt(5,compte.getId_devise());
-                   statement.setObject(6,compte.getType());
+                   statement.setObject(6,compte.getType().name(),Types.OTHER);
 
                    int rows = statement.executeUpdate();
 
@@ -79,7 +78,7 @@ public class comptesCrudOperations implements CrudOperations<comptes>{
     @Override
     public comptes save(comptes toSave) {
         try {
-            String sql = "INSERT INTO Comptes(id,nomCompte,solde,dateMisAJour,idDevise,type) VALUES(?,?,?,?,?,?);";
+            String sql = "INSERT INTO Compte(id,nom_de_compte,solde,solde_date_mis_a_jour,devise_id,type_compte) VALUES(?,?,?,?,?,?);";
 
             getConnection();
             try (PreparedStatement statement = connection.prepareStatement(sql)){
@@ -88,7 +87,7 @@ public class comptesCrudOperations implements CrudOperations<comptes>{
                 statement.setDouble(3,toSave.getSolde());
                 statement.setObject(4,toSave.getSolde_date_mis_a_jour());
                 statement.setInt(5,toSave.getId_devise());
-                statement.setObject(6,toSave.getType());
+                statement.setObject(6,toSave.getType(),Types.OTHER);
 
                 statement.executeUpdate();
             }
@@ -103,14 +102,14 @@ public class comptesCrudOperations implements CrudOperations<comptes>{
     @Override
     public comptes update(comptes toUpdate) {
         try {
-            String sql = "UPDATE compte SET nomCompte=?,solde=?,dateMisAJour=?,idDevise=?,type=? WHERE id=?;";
+            String sql = "UPDATE compte SET nom_de_compte=?,solde=?,solde_date_mis_a_jour=?,devise_id=?,type_compte=? WHERE id=?;";
 
             try (PreparedStatement statement = connection.prepareStatement(sql)){
                 statement.setString(1,toUpdate.getNom_de_compte());
                 statement.setDouble(2,toUpdate.getSolde());
                 statement.setObject(3,toUpdate.getSolde_date_mis_a_jour());
                 statement.setInt(4,toUpdate.getId_devise());
-                statement.setObject(5,toUpdate.getType());
+                statement.setObject(5,toUpdate.getType().name(), Types.OTHER);
                 statement.setInt(6,toUpdate.getId());
 
                 statement.executeUpdate();
---
+++ src/main/java/com/example/Walletpart2/Walletpart2/Repository/devisesCrudOperations.java
@@ -0,0 +1,110 @@
+package com.example.Walletpart2.Walletpart2.Repository;
+
+import com.example.Walletpart2.Walletpart2.Database.ConnectDatabase;
+import com.example.Walletpart2.Walletpart2.Model.CodeDevises;
+import com.example.Walletpart2.Walletpart2.Model.NomDevises;
+import com.example.Walletpart2.Walletpart2.Model.devises;
+
+import java.lang.reflect.Type;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class devisesCrudOperations implements CrudOperations<devises>{
+    private static Connection connection;
+    public static void getConnection() throws SQLException, ClassNotFoundException {
+        ConnectDatabase connectDatabase = new ConnectDatabase();
+        connection = connectDatabase.CreateConnection();
+    }
+    public static devises extractDevise(ResultSet resultSet) throws SQLException {
+       int id = resultSet.getInt("devise_id");
+        NomDevises nomDevises = NomDevises.valueOf(resultSet.getString("nomdevise"));
+        CodeDevises codeDevises = CodeDevises.valueOf(resultSet.getString("codedevise"));
+
+        return new devises(id,nomDevises,codeDevises);
+    }
+    @Override
+    public List<devises> findAll() throws SQLException, ClassNotFoundException {
+        List<devises> devises =new ArrayList<>();
+        String sql = "SELECT *FROM devises;";
+        getConnection();
+        try (PreparedStatement statement = connection.prepareStatement(sql)){
+           ResultSet resultSet = statement.executeQuery();
+           while (resultSet.next()) {
+                 devises devise = extractDevise(resultSet);
+                 devises.add(devise);
+           }
+            for (devises devise: devises){
+                System.out.println(devise);
+            }
+        }
+        return devises;
+    }
+
+    @Override
+    public List<devises> saveAll(List<devises> toSave) {
+        List<devises> devises = new ArrayList<>();
+        try {
+            String sql = "INSERT INTO devises(devise_id,nomdevise,codedevise)VALUES(?,?,?);";
+            getConnection();
+            try (PreparedStatement statement  = connection.prepareStatement(sql)){
+                for (devises devise: toSave){
+                    statement.setInt(1,devise.getId_devise());
+                    statement.setObject(2,devise.getNomDevises(), Types.OTHER);
+                    statement.setObject(3,devise.getCodeDevises(), Types.OTHER);
+
+                    int rows = statement.executeUpdate();
+                    while (rows>0){
+                        devises.add(devise);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return devises;
+    }
+
+    @Override
+    public devises save(devises toSave) {
+        try {
+            String sql ="INSERT INTO devises(devise_id,nomdevise,codedevise)VALUES(?,?,?);";
+
+            getConnection();
+            try (PreparedStatement statement = connection.prepareStatement(sql)){
+                statement.setInt(1,toSave.getId_devise());
+                statement.setObject(2,toSave.getNomDevises(),Types.OTHER);
+                statement.setObject(3,toSave.getCodeDevises(),Types.OTHER);
+
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    @Override
+    public devises update(devises toUpdate) {
+       try {
+           String sql = "UPDATE devises SET nomdevise=?,codedevise=? WHERE devise_id=?;";
+           getConnection();
+           try (PreparedStatement statement = connection.prepareStatement(sql)){
+               statement.setObject(1,toUpdate.getNomDevises(),Types.OTHER);
+               statement.setObject(2,toUpdate.getCodeDevises(),Types.OTHER);
+               statement.setInt(3,toUpdate.getId_devise());
+
+               statement.executeUpdate();
+           }
+       } catch (SQLException e) {
+           throw new RuntimeException(e);
+       } catch (ClassNotFoundException e) {
+           throw new RuntimeException(e);
+       }
+       return null;
+    }
+}
