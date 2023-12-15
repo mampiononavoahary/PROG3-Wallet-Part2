0824529aea81096cd4526f6a7139a9ecc31b89e2
Voahary Mampionona <hei.zoarisoa@gmail.com>
2023-12-15 12:54:18 +0300

    feat: add tests for function SQL Mapper

--- src/main/java/com/example/Walletpart2/Walletpart2/Tests/FunctionSqlGetAmountNetTests.java
+++ src/main/java/com/example/Walletpart2/Walletpart2/Tests/FunctionSqlGetAmountNetTests.java
@@ -1,2 +1,18 @@
-package com.example.Walletpart2.Walletpart2.Tests;public class FunctionSqlGetAmountNetTests {
+package com.example.Walletpart2.Walletpart2.Tests;
+
+import com.example.Walletpart2.Walletpart2.Service.FunctionSqlGetAmountNet;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+public class FunctionSqlGetAmountNetTests {
+    public static void FunctionSqlGetAmountNetTests() throws SQLException, ClassNotFoundException {
+        FunctionSqlGetAmountNet functionSqlGetAmountNet = new FunctionSqlGetAmountNet();
+
+        Timestamp dateDebut = Timestamp.valueOf("2023-10-10 23:53:27.778397");
+        Timestamp dateEnd = Timestamp.valueOf("2023-12-30 23:53:27.778397");
+
+        FunctionSqlGetAmountNet.getAmountNet(1,dateDebut,dateEnd);
+
+    }
 }
