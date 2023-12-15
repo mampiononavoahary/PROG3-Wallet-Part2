709d39d90544005e00e895d8acf8ca21008b97b2
Voahary Mampionona <hei.zoarisoa@gmail.com>
2023-12-15 12:16:04 +0300

    feat: add tests to get amount net

--- src/main/java/com/example/Walletpart2/Walletpart2/Tests/GetMontantNetTests.java
+++ src/main/java/com/example/Walletpart2/Walletpart2/Tests/GetMontantNetTests.java
@@ -1,2 +1,17 @@
-package com.example.Walletpart2.Walletpart2.Tests;public class GetMontantNetTests {
+package com.example.Walletpart2.Walletpart2.Tests;
+
+import com.example.Walletpart2.Walletpart2.Service.GetSumCreditDebit;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+public class GetMontantNetTests {
+    public static void GetMontantNetTests() throws SQLException, ClassNotFoundException {
+     GetSumCreditDebit getSumCreditDebit = new GetSumCreditDebit();
+        Timestamp dateDebut = Timestamp.valueOf("2023-10-10 23:53:27.778397");
+        Timestamp dateEnd = Timestamp.valueOf("2023-12-30 23:53:27.778397");
+        GetSumCreditDebit.GetSumCreditDebit(1,dateDebut,dateEnd);
+
+    }
+
 }
