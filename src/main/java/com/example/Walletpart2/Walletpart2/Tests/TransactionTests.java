8f382efe225a81fd26e215ceecb0f1ca7e25feba
Voahary Mampionona <hei.zoarisoa@gmail.com>
2023-12-15 14:57:36 +0300

    feat: finished project

--- src/main/java/com/example/Walletpart2/Walletpart2/Tests/TransactionTests.java
+++ src/main/java/com/example/Walletpart2/Walletpart2/Tests/TransactionTests.java
@@ -2,7 +2,9 @@ package com.example.Walletpart2.Walletpart2.Tests;
 
 import com.example.Walletpart2.Walletpart2.Model.comptes;
 import com.example.Walletpart2.Walletpart2.Model.transactions;
+import com.example.Walletpart2.Walletpart2.Model.typeCompte;
 import com.example.Walletpart2.Walletpart2.Model.typeTransactions;
+import com.example.Walletpart2.Walletpart2.Repository.comptesCrudOperations;
 import com.example.Walletpart2.Walletpart2.Repository.transactionsCrudOperations;
 
 import java.sql.SQLException;
@@ -13,16 +15,23 @@ import java.util.List;
 
 public class TransactionTests {
         public static void transactionTest() throws SQLException, ClassNotFoundException {
+            comptesCrudOperations comptesCrudOperations = new comptesCrudOperations();
+            comptes compte = new comptes(1,"compte courant",2000.0, LocalDateTime.now(),1, typeCompte.BANK);
             compteTests compteTests;
             LocalDateTime updatedDate = LocalDateTime.now();
 
 
-            Double value = 20000.00;
-            transactions transaction = new transactions(1,1,"cadeaux de Noel", value,updatedDate,typeTransactions.CREDIT);
+            Double value = 500.00;
+            transactions transaction = new transactions(1,1,"cadeaux de Noel", value,updatedDate,typeTransactions.DEBIT,1);
+
+
+            Double debiter = 20000.00;
+            transactions transaction2 = new transactions(2,1,"Tax", debiter,updatedDate,typeTransactions.CREDIT,2);
             transactionsCrudOperations test = new transactionsCrudOperations();
 
-            //test.CangeCompteAfterTransactions();
-            test.save(transaction);
+            test.CangeCompteAfterTransactions(compte,transaction);
+            test.CangeCompteAfterTransactions(compte,transaction2);
+            //test.save(transaction);
             test.findAll();
         }
 }
